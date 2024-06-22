9f1e45fc6020581b98600408f81893f9085ae863
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-06-22 13:10:01 +0530

    chore: update chart tests to handle prometheus url

--- test_charts.sh
+++ test_charts.sh
@@ -1,3 +1,8 @@
 for chart in $(ls charts); do
     helm template charts/$chart
+
+    # kubernetes chart renders differently, if prometheus.url is set
+    if [[ $chart == "kubernetes" ]]; then
+        helm template --set prometheus.url='http://prometheus' charts/$chart
+    fi
 done
