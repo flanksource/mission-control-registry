6dd12f4d20dd7b6c2a1bc473cf9b8c8fa67bc01a
Moshe Immermam <moshe@flanksource.com>
2024-09-17 13:40:22 +0300

    chore: pickup unknown field errors

--- test_charts.sh
+++ test_charts.sh
@@ -3,14 +3,27 @@ helm repo add flanksource https://flanksource.github.io/charts
 helm repo update
 helm install --devel mission-control-crds flanksource/mission-control-crds --wait
 
+function testChart() {
+    cd $1
+    ct lint-and-install  --charts . --namespace default  2>&1 > stdout.log
+    exitCode=$?
+    cat stdout.log
+
+    if [[ "$exitCode" != "0" ]]; then
+        echo "ct-lint-and install failed"
+        exit 1
+    fi
+    if grep "unknown field" stdout.log; then
+        echo "Unknown field detected"
+        exit 1
+    fi
+}
+
 if [[ "$1" == "" ]]; then
     dir=$(PWD)
     for chart in $(ls charts); do
-        cd $dir/charts/$chart
-      ct lint-and-install  --charts .   --namespace default
+      testChart $dir/charts/$chart
     done
-
 else
-    cd charts/$1
-    ct lint-and-install  --charts . --namespace default
+    testChart charts/$1
 fi
