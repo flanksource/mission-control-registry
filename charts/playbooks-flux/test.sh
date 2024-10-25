acb89835d544e83e5b42c4760ad969ea7f5889f0
Yash Mehrotra <yashmehrotra95@gmail.com>
2024-10-25 11:53:13 +0530

    chore: fix templates for kustomize

--- charts/playbooks-flux/test.sh
+++ charts/playbooks-flux/test.sh
@@ -1,8 +1,10 @@
 # Config db run more than once for all relationships to form
+# Also refresh config_summary_7d materialized view
 # system user's email in people table should be set for git
 # Run incident-commander in serve mode for git connection to by synced
 # All of these should be run in AWS Cluster pointed in kubeconfig
 
+#!/bin/bash
 set -x
 function runPlaybook() {
   all_args=("$@")
@@ -15,15 +17,49 @@ function runPlaybook() {
   #fi
 }
 
-REPO_ORIGINAL_COMMIT="eb127e4d03bbae22128d53908e4a501b17d19677"
+function runPlaybookNoEditMain() {
+  all_args=("$@")
+  echo "Running playbook $1 ${all_args[@]:1}"
+  #helm template ./ --set git.connection="connection://mission-control/github" --set createPullRequest=false | yq ea " . | select(.metadata.name == \"$1\")" | yq "... comments=\"\" " | incident-commander playbook run -v=5  /dev/stdin ${all_args[@]:1}
+  helm template ./ --set git.connection="connection://mission-control/github" --set createPullRequest=false | yq ea " . | select(.metadata.name == \"$1\")"  | incident-commander playbook run -v=5  /dev/stdin ${all_args[@]:1}
+
+  # TODO: Fix playbook cmd ret code
+  #if [[ "$?" != "0" ]]; then
+    #exit 1
+  #fi
+}
+
+REPO_ORIGINAL_COMMIT="4a416ec6dbf5e82f6833cad1c21626ead2ed3791"
+
+fileroot=$(pwd)
 
 name=flux-$(date "+%H%M%S")
 kustomization=$(kubectl -n mission-control exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::Kustomization name=flux-playbooks-test -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
+helmrelease=$(kubectl -n mission-control exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::HelmRelease name=echo-server -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
 
 echo $kustomization
 
+# Reconcile,suspend, resume
+echo "Running flux-suspend ==================="
+runPlaybookNoEditMain "flux-suspend" config=$helmrelease
+echo "Verifying flux-suspend ================="
+if [[ $(flux get hr -n playbooks-test --no-header | awk '{print $3}') != "True" ]]; then
+  echo "Error: Flux suspend playbook failed"
+  exit 1
+fi
+
+
+echo "Running flux-resume =================="
+runPlaybookNoEditMain "flux-resume" config=$helmrelease
+echo "Verifying flux-resume =================="
+if [[ $(flux get hr -n playbooks-test --no-header | awk '{print $3}') != "False" ]]; then
+  echo "Error: Flux resume playbook failed"
+  exit 1
+fi
+
+runPlaybookNoEditMain "flux-reconcile" config=$helmrelease
+
 # Helm related playbooks
-helmrelease=$(kubectl -n mission-control exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::HelmRelease name=echo-server -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
 
 echo "Running Helm update-values and version =================="
 echo '{"yamlInput": "replicaCount: 3", "commit_message": "From local helm playbook test"}' > /tmp/params-update-helm-values
@@ -47,33 +83,50 @@ fi
 
 # Reset source repo
 echo "Resetting to last stable commit"
-cd /tmp/playbooks-test && git reset --hard $REPO_ORIGINAL_COMMIT && git push -f origin main && cd -
+cd /tmp/playbooks-test && git reset --hard $REPO_ORIGINAL_COMMIT && git push -f origin main
+cd $fileroot
 rm -rf /tmp/playbooks-test
 
+# Create deployment
+namespace=$(kubectl -n mission-control exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::Namespace name=playbooks-test -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
+runPlaybookNoEditMain "kustomize-create-deployment" config=$namespace name="nginx" image="nginx:latest"
 
+# Create namespace
+runPlaybookNoEditMain "kustomize-create-namespace" name="playbook-test-ns" config=$kustomization
 
-# Reconcile,suspend, resume
-echo "Running flux-suspend ==================="
-runPlaybook "flux-suspend" config=$helmrelease
-echo "Verifying flux-suspend ================="
-if [[ $(flux get hr -n playbooks-test --no-header | awk '{print $3}') != "True" ]]; then
-  echo "Error: Flux suspend playbook failed"
+git clone git@github.com:flanksource/flux-playbooks-test.git /tmp/playbooks-test
+if [[ $(cat /tmp/playbooks-test/kustomization.yaml | yq '.resources | contains(["deploy-nginx.yaml"])') != 'true' ]]; then
+  echo "Error: Create deployment playbook failed"
   exit 1
 fi
 
-
-echo "Running flux-resume =================="
-runPlaybook "flux-resume" config=$helmrelease
-echo "Verifying flux-resume =================="
-if [[ $(flux get hr -n playbooks-test --no-header | awk '{print $3}') != "False" ]]; then
-  echo "Error: Flux resume playbook failed"
+if [[ $(cat /tmp/playbooks-test/deploy-nginx.yaml | yq '.spec.template.spec.containers[0].image') != 'nginx:latest' ]]; then
+  echo "Error: Create deployment playbook failed"
   exit 1
 fi
 
-runPlaybook "flux-reconcile" config=$helmrelease
+if [[ $(cat /tmp/playbooks-test/kustomization.yaml | yq '.resources | contains(["namespaces/playbook-test-ns"])') != 'true' ]]; then
+  echo "Error: Create namespace playbook failed"
+  exit 1
+fi
+
+if [[ $(cat /tmp/playbooks-test/namespaces/playbook-test-ns/kustomization.yaml | yq '.resources | contains(["namespace.yaml"])') != 'true' ]]; then
+  echo "Error: Create namespace playbook failed"
+  exit 1
+fi
+
+if [[ $(cat /tmp/playbooks-test/namespaces/playbook-test-ns/namespace.yaml | yq '.metadata.name ') != 'playbook-test-ns' ]]; then
+  echo "Error: Create namespace playbook failed"
+  exit 1
+fi
+
+# Reset source repo
+echo "Resetting to last stable commit"
+cd /tmp/playbooks-test && git reset --hard $REPO_ORIGINAL_COMMIT && git push -f origin main
+cd $fileroot
+rm -rf /tmp/playbooks-test
 
 # Namespace related playbooks
-namespace=$(kubectl exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::Namespace name=httpbin -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
 role=$(kubectl exec -it deploy/mission-control -- ./incident-commander catalog query type=Kubernetes::ClusterRole name=view -w 0s --log-to-stderr -f json | rg -v INF | jq -r '.configs[0].id')
 #runPlaybook "flux-request-namespace-access" delay="4h" role=$role config=$namespace
 
