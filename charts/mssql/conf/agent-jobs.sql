30c4ecfd3a1b533d21a2ca4f0d71e3773755a351
Moshe Immerman <moshe@flanksource.com>
2025-12-05 12:12:44 +0200

    chore: fix mssql agent jobs

--- charts/mssql/conf/agent-jobs.sql
+++ charts/mssql/conf/agent-jobs.sql
@@ -1,4 +1,6 @@
-CREATE TABLE #job_steps (
+-- Create temp table for job steps
+CREATE TABLE #JobSteps (
+    job_id UNIQUEIDENTIFIER,
     step_id INT,
     step_name NVARCHAR(128),
     subsystem NVARCHAR(40),
@@ -21,35 +23,37 @@ CREATE TABLE #job_steps (
     last_run_retries INT,
     last_run_date INT,
     last_run_time INT,
-    proxy_id INT,
-    job_id UNIQUEIDENTIFIER
+    proxy_id INT
 );
 
+-- Insert job step data using sp_help_jobstep for each job
 DECLARE @job_id UNIQUEIDENTIFIER;
-DECLARE @job_name NVARCHAR(128);
-
 DECLARE job_cursor CURSOR FOR
-SELECT job_id, name FROM msdb.dbo.sysjobs WHERE enabled = 1;
+    SELECT job_id FROM msdb.dbo.sysjobs WHERE enabled = 1;
 
 OPEN job_cursor;
-FETCH NEXT FROM job_cursor INTO @job_id, @job_name;
+FETCH NEXT FROM job_cursor INTO @job_id;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
     BEGIN TRY
-        INSERT INTO #job_steps (step_id, step_name, subsystem, command, flags, cmdexec_success_code,
-            on_success_action, on_success_step_id, on_fail_action, on_fail_step_id, server,
-            database_name, database_user_name, retry_attempts, retry_interval, os_run_priority,
-            output_file_name, last_run_outcome, last_run_duration, last_run_retries, last_run_date, last_run_time, proxy_id)
+        INSERT INTO #JobSteps (step_id, step_name, subsystem, command, flags, cmdexec_success_code,
+                              on_success_action, on_success_step_id, on_fail_action, on_fail_step_id,
+                              server, database_name, database_user_name, retry_attempts, retry_interval,
+                              os_run_priority, output_file_name, last_run_outcome, last_run_duration,
+                              last_run_retries, last_run_date, last_run_time, proxy_id)
         EXEC msdb.dbo.sp_help_jobstep @job_id = @job_id;
 
-        UPDATE #job_steps SET job_id = @job_id WHERE job_id IS NULL;
+        -- Update the job_id for all rows we just inserted
+        UPDATE #JobSteps SET job_id = @job_id WHERE job_id IS NULL;
     END TRY
     BEGIN CATCH
+        -- If sp_help_jobstep fails for this job, just skip it and continue
+        PRINT 'Skipping job_id: ' + CAST(@job_id AS VARCHAR(50)) + ' - ' + ERROR_MESSAGE();
     END CATCH
 
-    FETCH NEXT FROM job_cursor INTO @job_id, @job_name;
-END
+    FETCH NEXT FROM job_cursor INTO @job_id;
+END;
 
 CLOSE job_cursor;
 DEALLOCATE job_cursor;
@@ -57,7 +61,7 @@ DEALLOCATE job_cursor;
 WITH LastJobRuns AS (
     SELECT job_id, MAX(instance_id) AS last_instance_id
     FROM msdb.dbo.sysjobhistory WITH (NOLOCK)
-    WHERE step_id != 0
+    WHERE step_id = 0
     GROUP BY job_id
 ),
 JobStatus AS (
@@ -76,9 +80,19 @@ JobStatus AS (
     LEFT JOIN LastJobRuns l ON j.job_id = l.job_id
     LEFT JOIN msdb.dbo.sysjobhistory h WITH (NOLOCK) ON h.job_id = l.job_id AND h.instance_id = l.last_instance_id
     WHERE j.enabled = 1
+),
+JobStepsAgg AS (
+    SELECT
+        js.job_id,
+        STRING_AGG(js.command, CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10))
+            WITHIN GROUP (ORDER BY js.step_id) as sql
+    FROM #JobSteps js
+    WHERE js.subsystem = 'TSQL'
+    GROUP BY js.job_id
 )
 SELECT
     js.job_name,
+    lower(CAST(js.job_id AS NVARCHAR(36))) as job_id,
     CASE
         WHEN js.run_status = 0 THEN 'Failed'
         WHEN js.run_status = 1 THEN 'Succeeded'
@@ -91,31 +105,12 @@ SELECT
     js.duration_formatted as duration,
     js.duration_seconds,
     js.job_message,
-    CASE WHEN js.run_status = 0 THEN 1 ELSE 0 END as is_failed,
-    (
-        SELECT step_id, step_name, subsystem, command, database_name,
-               CASE on_success_action
-                   WHEN 1 THEN 'Quit with success'
-                   WHEN 2 THEN 'Quit with failure'
-                   WHEN 3 THEN 'Go to next step'
-                   WHEN 4 THEN 'Go to step ' + CAST(on_success_step_id AS VARCHAR(10))
-                   ELSE 'Unknown'
-               END as on_success_action,
-               CASE on_fail_action
-                   WHEN 1 THEN 'Quit with success'
-                   WHEN 2 THEN 'Quit with failure'
-                   WHEN 3 THEN 'Go to next step'
-                   WHEN 4 THEN 'Go to step ' + CAST(on_fail_step_id AS VARCHAR(10))
-                   ELSE 'Unknown'
-               END as on_fail_action,
-               retry_attempts, retry_interval
-        FROM #job_steps s
-        WHERE s.job_id = js.job_id
-        ORDER BY s.step_id
-        FOR JSON PATH
-    ) as steps
+    COALESCE(jst.sql, '') as sql
 FROM JobStatus js
-WHERE job_name != 'syspolicy_purge_history'
-ORDER BY js.run_datetime DESC;
+LEFT JOIN JobStepsAgg jst ON js.job_id = jst.job_id;
 
-DROP TABLE #job_steps;
+-- Clean up temp table
+DROP TABLE #JobSteps;
+
+
+
